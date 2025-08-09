3e2fb4740d8aa00b6eeff974e5ebf47d5bfb892b
TaiwoDaniel <taiwodaniel3116@gmail.com>
2025-08-09 10:11:36 -0700

    removed src from image file path

--- src/data/product.js
+++ src/data/product.js
@@ -2,183 +2,183 @@
 export const products = [
   {
    id: "1",
-  //  image: "src/images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-   image: "public/images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+  //  image: "/images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+   image: "/images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
    name: "Adult Cotton T-Shirt",
    price: 200,
    quantity: 1
   },
   {
    id: "2",
-   image: "src/images/backpack.jpg",
+   image: "/images/backpack.jpg",
    name: "  Black Backpack",
    price: 65,
    quantity: 1
   },
   {
    id: "3",
-   image: "src/images/black-2-slot-toaster.jpg",
+   image: "/images/black-2-slot-toaster.jpg",
    name: "Black Toaster",
    price: 250,
    quantity: 1
   },
   {
    id: "4",
-   image: "src/images/coffeemaker-with-glass-carafe-black.jpg",
+   image: "/images/coffeemaker-with-glass-carafe-black.jpg",
    name: "Coffee Maker",
    price: 100,
    quantity: 1
   },
   {
    id: "5",
-   image: "src/images/knit-athletic-sneakers-pink.webp",
+   image: "/images/knit-athletic-sneakers-pink.webp",
    name: "Athletic Sneakers",
    price: 110,
    quantity: 1
   },
   {
    id: "6",
-   image: "src/images/round-airtight-food-storage-containers.jpg",
+   image: "/images/round-airtight-food-storage-containers.jpg",
    name: "Food Container",
    price: 25,
    quantity: 1
   },
   {
    id: "7",
-   image: "src/images/duvet-cover-set-blue-twin.jpg",
+   image: "/images/duvet-cover-set-blue-twin.jpg",
    name: "Duvet Cover",
    price: 125,
    quantity: 1
   },
   {
    id: "8",
-   image: "src/images/electric-glass-and-steel-hot-water-kettle.webp",
+   image: "/images/electric-glass-and-steel-hot-water-kettle.webp",
    name: "Electric glass kettle",
    price: 45,
    quantity: 1
   },
   {
    id: "9",
-   image: "src/images/floral-mixing-bowl-set.jpg",
+   image: "/images/floral-mixing-bowl-set.jpg",
    name: "Floral Mixing Bowl",
    price: 70,
    quantity: 1
   },
   {
    id: "10",
-   image: "src/images/liquid-laundry-detergent-plain.jpg",
+   image: "/images/liquid-laundry-detergent-plain.jpg",
    name: "laundry detergent",
    price: 20,
    quantity: 1
   },
   {
    id: "11",
-   image: "src/images/non-stick-cooking-set-15-pieces.webp",
+   image: "/images/non-stick-cooking-set-15-pieces.webp",
    name: "Cooking Set",
    price: 35,
    quantity: 1
   },
   {
    id: "12",
-   image: "src/images/straw-sunhat.webp",
+   image: "/images/straw-sunhat.webp",
    name: "Straw Sunhat",
    price: 25,
    quantity: 1
   },
   {
    id: "13",
-   image: "src/images/women-knit-ballet-flat-black.jpg",
+   image: "/images/women-knit-ballet-flat-black.jpg",
    name: "Knit ballet",
    price: 35,
    quantity: 1
   },
   {
    id: "14",
-   image: "src/images/women-stretch-popover-hoodie-black.jpg",
+   image: "/images/women-stretch-popover-hoodie-black.jpg",
    name: "Popover hoodie",
    price: 25,
    quantity: 1
   },
   {
    id: "15",
-   image: "src/images/women-french-terry-fleece-jogger-camo.jpg",
+   image: "/images/women-french-terry-fleece-jogger-camo.jpg",
    name: "Fleece Jogger Camo",
    price: 70,
    quantity: 1
   },
   {
    id: "16",
-   image: "src/images/women-chunky-beanie-gray.webp",
+   image: "/images/women-chunky-beanie-gray.webp",
    name: "Chunky Beanie",
    price: 15,
    quantity: 1
   },
   {
    id: "17",
-   image: "src/images/women-chiffon-beachwear-coverup-black.jpg",
+   image: "/images/women-chiffon-beachwear-coverup-black.jpg",
    name: "Chiffon Beachwear",
    price: 30,
    quantity: 1
   },
   {
    id: "18",
-   image: "src/images/vanity-mirror-silver.jpg",
+   image: "/images/vanity-mirror-silver.jpg",
    name: "Vanity Silver Mirror",
    price: 10,
    quantity: 1
   },
   {
    id: "19",
-   image: "src/images/trash-can-with-foot-pedal-50-liter.jpg",
+   image: "/images/trash-can-with-foot-pedal-50-liter.jpg",
    name: "Trash Can",
    price: 10,
    quantity: 1
   },
   {
    id: "20",
-   image: "src/images/umbrella.jpg",
+   image: "/images/umbrella.jpg",
    name: "Umbrella",
    price: 5,
    quantity: 1
   },
   {
    id: "21",
-   image: "src/images/men-slim-fit-summer-shorts-gray.jpg",
+   image: "/images/men-slim-fit-summer-shorts-gray.jpg",
    name: "Men Shorts",
    price: 55,
    quantity: 1
   },
   {
    id: "22",
-   image: "src/images/round-sunglasses-black.jpg",
+   image: "/images/round-sunglasses-black.jpg",
    name: "Sunglasses",
    price: 8,
    quantity: 1
   },
   {
    id: "23",
-   image: "src/images/plain-hooded-fleece-sweatshirt-yellow.jpg",
+   image: "/images/plain-hooded-fleece-sweatshirt-yellow.jpg",
    name: "Plain Hoodie",
    price: 50,
    quantity: 1
   },
    {
     id: "24",
-    image: "src/images/men-golf-polo-t-shirt-blue.jpg",
+    image: "/images/men-golf-polo-t-shirt-blue.jpg",
     name: "Golf Polo",
     price: 52,
     quantity: 1
    },
    {
     id: "25",
-    image: "src/images/knit-athletic-sneakers-gray.jpg",
+    image: "/images/knit-athletic-sneakers-gray.jpg",
     name: "Black Sneakers",
     price: 30,
     quantity: 1
    },
    {
     id: "26",
-    image: "src/images/blackout-curtain-set-beige.webp",
+    image: "/images/blackout-curtain-set-beige.webp",
     name: "Blackout Curtain",
     price: 25,
     quantity: 1
